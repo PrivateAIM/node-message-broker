6aef055501846bd8fc0fecb80272903bab1d49c9
Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>
2024-02-26 16:56:21 +0100

    fix: remove superfluous configuration
    Removes a superfluous configuration from the index file.
    This is already taken care of within the context of the
    server.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
 import { ServerModule } from './server/server.module';
 
 async function bootstrap() {
     const analysisCommunicationServer = await NestFactory.create(ServerModule);
-
-    // for validating request data
-    analysisCommunicationServer.useGlobalPipes(
-        new ValidationPipe({
-            transform: true,
-            always: true,
-        }),
-    );
     const config = analysisCommunicationServer.get(ConfigService);
     await analysisCommunicationServer.listen(config.getOrThrow<string>('serverPort'));
 }
