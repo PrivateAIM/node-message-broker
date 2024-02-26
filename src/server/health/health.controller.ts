656f19a4c099e1398995f2a977aee0c8d8220c6b
Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>
2024-02-26 17:06:01 +0100

    feat: integrate db into healthcheck
    Checks the availability of the underlying database
    and reports the status back to the overall health
    check of the server.

--- src/server/health/health.controller.ts
+++ src/server/health/health.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, MongooseHealthIndicator } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
     private readonly healthService: HealthCheckService;
 
-    constructor(healthService: HealthCheckService) {
+    private readonly db: MongooseHealthIndicator;
+
+    constructor(healthService: HealthCheckService, db: MongooseHealthIndicator) {
         this.healthService = healthService;
+        this.db = db;
     }
 
     @Get()
     @HealthCheck()
     check() {
-        return this.healthService.check([]);
+        return this.healthService.check([
+            async () => this.db.pingCheck('db'),
+        ]);
     }
 }
