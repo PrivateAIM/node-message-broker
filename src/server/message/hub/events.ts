0270ea29f89b6d2c671039b1506d4f42c42ee453
Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>
2024-07-31 13:39:23 +0200

    chore: remove previously used nestjs implementation
    Removes the previously used nestjs implementation that has
    been refactored using Effect.
    
    Signed-off-by: Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>

--- src/server/message/hub/events.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Describes an event that is fired when a message from the central side (hub)
- * is received.
- */
-export class HubMessageBroadcastEvent {
-    analysisId: string;
-
-    messagePayload: Record<string, any>;
-}
