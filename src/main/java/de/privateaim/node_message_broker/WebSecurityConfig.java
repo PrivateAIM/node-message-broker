ce772cb1561a0e6291b7d519dd45fe15b2d070aa
Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>
2024-11-07 10:46:31 +0100

    Require Authentication for all Requests
    All requests are required to be authenticated. This also includes
    health endpoints (will be changed later on).
    Thus, a client needs to provide a valid JWT in order for the
    request to get through. Validation of this token is accomplished
    by leveraging an OpenID provider using its JWKS URL. Via the same
    URL the application automatically runs discovery processes regarding
    the provider.
    
    Signed-off-by: Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>

--- src/main/java/de/privateaim/node_message_broker/WebSecurityConfig.java
+++ src/main/java/de/privateaim/node_message_broker/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package de.privateaim.node_message_broker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -12,11 +13,20 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity(securedEnabled = true, prePostEnabled = false)
 public class WebSecurityConfig {
 
+    @Value("${app.auth.jwksUrl}")
+    private String jwksUrl;
+
     @Bean
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         // TODO: do not keep this open in production -> revise after overhaul
         return http.authorizeHttpRequests(req ->
-                        req.anyRequest().permitAll())
+                        req.anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri(jwksUrl)
+                        )
+                )
                 .build();
     }
 }
