87427995b6f563551dbb332d27c9e2a0c33ec076
Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>
2024-12-11 21:26:38 +0100

    Fixup Hub Client Exception Visibility
    Signed-off-by: Alexander Twrdik <6052859+DiCanio@users.noreply.github.com>

--- src/main/java/de/privateaim/node_message_broker/common/hub/HubAnalysisNodesNotObtainable.java
+++ src/main/java/de/privateaim/node_message_broker/common/hub/HubAnalysisNodesNotObtainable.java
@@ -3,7 +3,7 @@ package de.privateaim.node_message_broker.common.hub;
 /**
  * Signals that analysis nodes could not be obtained from the hub core services.
  */
-public class HubAnalysisNodesNotObtainable extends RuntimeException {
+public final class HubAnalysisNodesNotObtainable extends RuntimeException {
     public HubAnalysisNodesNotObtainable(String message) {
         super(message);
     }
--- src/main/java/de/privateaim/node_message_broker/common/hub/HubCoreServerException.java
+++ src/main/java/de/privateaim/node_message_broker/common/hub/HubCoreServerException.java
@@ -4,7 +4,7 @@ package de.privateaim.node_message_broker.common.hub;
  * Signals that a request to a central Hub service has failed due to a server error.
  * An operation causing this error might be retryable.
  */
-public class HubCoreServerException extends RuntimeException {
+final class HubCoreServerException extends RuntimeException {
     public HubCoreServerException(String message) {
         super(message);
     }
